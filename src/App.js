5843458b9c8f8e47766c0f5c7f73931a0f980854
Luke <lukamwangi.k@gmail.com>
2022-03-22 09:41:06 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomersList from "./CustomersList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter } from "react-router-dom";
+import NoMatchPage from "./NoMatchPage";
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <NavBar />
+        <div className="container-fluid">
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/customers" element={<CustomersList />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+            <Route path="*" element={<NoMatchPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
